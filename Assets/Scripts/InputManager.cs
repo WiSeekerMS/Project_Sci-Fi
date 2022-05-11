c2de502536fc2997702e031742174c4422d21131
Maxim <maxsim-troizk@yandex.ru>
2022-05-11 22:48:52 +0500

    Commit

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Assets.Scripts
 {
     public class InputManager : MonoBehaviour
     {
+        //private InputActionAsset touchControlls;
         public static InputManager Instance;
 
         private void Awake()
         {
             Instance = this;
+            //touchControlls = new InputActionAsset();
+            //touchControl
         }
+
+        private void OnTap(InputAction.CallbackContext context){}
     }
 }
